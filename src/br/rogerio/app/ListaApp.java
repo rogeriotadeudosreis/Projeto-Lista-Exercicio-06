e7f8da393da1e8b4a3d48030802ec6e9f73e2ded
Rogério Tadeu dos Reis - ADS Senai <rogerio.rtr46@gmail.com>
2020-11-16 17:56:21 -0300

    Primeiro commit deste projeto

---
+++ src/br/rogerio/app/ListaApp.java
@@ -0,0 +1,48 @@
+/*
+ * 6. Escreva o método de inserção para uma lista ordenada implementada em
+vetores. Seu método de inserção deverá utilizar um método de busca binária
+para verificar se o elemento existe ou não. Caso o elemento não exista a sua
+busca binária deverá retornar a posição onde o elemento deverá ser
+inserido.
+ */
+package br.rogerio.app;
+
+import br.rogerio.model.Lista;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author roger
+ */
+public class ListaApp {
+
+    public static void main(String[] args) {
+
+        Lista lista = new Lista(10);
+        try {
+            lista.insereInicio(9);
+            lista.insereInicio(13);
+            lista.insereInicio(27);
+            lista.insereInicio(31);
+            lista.insereInicio(47);
+            lista.insereInicio(67);
+            lista.insereInicio(71);
+            lista.insereInicio(83);
+            
+            lista.mostrarLista();
+            lista.ordenarLista();
+            lista.mostrarLista();
+
+            lista.inserirComVerificacao(47);
+            lista.mostrarLista();
+            lista.inserirComVerificacao(51);
+            lista.mostrarLista();
+            lista.inserirComVerificacao(51);
+
+        } catch (RuntimeException erro) {
+            JOptionPane.showMessageDialog(null, erro.getMessage());
+        }
+       
+    }
+
+}
---
+++ src/br/rogerio/model/Lista.java
@@ -0,0 +1,237 @@
+/*
+ * 6. Escreva o método de inserção para uma lista ordenada implementada em
+vetores. Seu método de inserção deverá utilizar um método de busca binária
+para verificar se o elemento existe ou não. Caso o elemento não exista a sua
+busca binária deverá retornar a posição onde o elemento deverá ser
+inserido.
+ */
+package br.rogerio.model;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author roger
+ */
+public class Lista {
+
+    private final int MAXTAM;
+    private int array[];
+    private int ultimo;
+
+    public Lista(int tamanho) {
+        if (tamanho <= 0) {
+            throw new RuntimeException("Tamanho deve ser maior que zero!\n");
+        }
+        MAXTAM = tamanho;
+        array = new int[MAXTAM];
+        ultimo = -1;
+    }
+
+    public boolean isEmpty() {
+        return ultimo == -1;
+    }
+
+    public boolean isFull() {
+        return ultimo + 1 == MAXTAM;
+    }
+
+    public int size() {
+        return ultimo + 1;
+    }
+
+    public void insereInicio(int elemento) {
+        if (isFull()) {
+            throw new RuntimeException("A lista está cheia!\n");
+        }
+        ultimo++;
+        for (int i = ultimo; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+        array[0] = elemento;
+    }
+
+    public void ordenarLista() {
+        for (int i = 0; i < ultimo; i++) {
+            for (int j = i + 1; j < ultimo + 1; j++) {
+                if (array[i] > array[j]) {
+                    int aux = array[i];
+                    array[i] = array[j];
+                    array[j] = aux;
+                }
+            }
+        }
+
+    }
+
+    public void inserirDeFormaOrdenada(int elemento) {
+        if (isFull()) {
+            throw new RuntimeException("A lista está cheia");
+        }
+        ultimo++;
+        int i = ultimo;
+        while ((i > 0) && (elemento < array[i - 1])) {
+            array[i] = array[i - 1];
+            i--;
+        }
+        array[i] = elemento;
+    }
+
+    public void mostrarLista() {
+        if (isEmpty()) {
+            throw new RuntimeException("A lista está vazia!\n");
+        }
+        System.out.println("*****  Elementos da Lista  *****");
+        for (int i = 0; i <= ultimo; i++) {
+            System.out.printf("%d ", array[i]);
+        }
+        System.out.println("");
+    }
+
+    public void insereFim(int elemento) {
+        if (isFull()) {
+            throw new RuntimeException("A lista está cheia!\n");
+        }
+        ultimo++;
+        array[ultimo] = elemento;
+    }
+
+    public void inserePosicao(int posicao, int elemento) {
+        if (isFull()) {
+            throw new RuntimeException("A lista está cheia!\n");
+        }
+        if (posicao > ultimo + 1 || posicao < 0) {
+            throw new RuntimeException("Posição inválida!\n");
+        }
+        ultimo++;
+        for (int i = ultimo; i > posicao; i--) {
+            array[i] = array[i - 1];
+        }
+        array[posicao] = elemento;
+    }
+
+    public void removeInicio() {
+        if (isEmpty()) {
+            throw new RuntimeException("A lista está vazia!\n");
+        }
+        for (int i = 0; i < 10; i++) {
+            array[i] = array[i + 1];
+        }
+        ultimo--;
+    }
+
+    public void removeFim() {
+        if (isEmpty()) {
+            throw new RuntimeException("A lista está vazia!\n");
+        }
+        ultimo--;
+    }
+
+    public void removePosicao(int posicao) {
+        if (isEmpty()) {
+            throw new RuntimeException("A lista está vazia!\n");
+        }
+        if (posicao < 0 || posicao > ultimo) {
+            throw new RuntimeException("Posição inválida!\n");
+        }
+        for (int i = posicao; i < ultimo; i++) {
+            array[i] = array[i + 1];
+        }
+        ultimo--;
+    }
+
+    //retorna a posição do elemento, caso o elemento exista
+    //retorna -1, caso contrário elemento não exista
+    public int buscaElemento(int elemento) {
+        for (int i = 0; i < ultimo; i++) {
+            if (array[i] == elemento) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    //retorna true, caso o elemento seja removido
+    //ou retorna false, caso contrário, se o elemento não foi removido
+    public boolean removeElemento(int elemento) {
+        int pos = buscaElemento(elemento);
+        if (pos == -1) {
+            return false;
+        }
+        removePosicao(pos);
+        return true;
+    }
+
+    public int getInicio() {
+        if (isEmpty()) {
+            throw new RuntimeException("A lista está vazia!\n");
+        }
+        return array[0];
+    }
+
+    public int getFim() {
+        if (isEmpty()) {
+            throw new RuntimeException("A lista está vazia!\n");
+        }
+        return array[ultimo];
+    }
+
+    public int getPosicao(int posicao) {
+        if (isEmpty()) {
+            throw new RuntimeException("A lista está vazia!\n");
+        }
+        if (posicao > ultimo || posicao < 0) {
+            throw new RuntimeException("Posição inválida!\n");
+        }
+        return array[posicao];
+    }
+
+    //tornando a lista vazia
+    public void destroy() {
+        ultimo = -1;
+    }
+
+    public void inserirComVerificacao(int elemento) {
+        if (isFull()) {
+            throw new RuntimeException("A lista está cheia");
+        }
+        int posicao = BuscaBinaria(elemento);
+        ultimo++;
+        
+        array[posicao] = elemento;
+        
+        System.out.println("Posição vazia da lista: " + posicao);
+
+    }
+
+    public int BuscaBinaria(int numero) {
+
+        int inicio = 0;
+        int fim = ultimo;
+        int meio = (inicio + fim) / 2;
+        boolean localizador = false;
+        while (inicio < fim && localizador == false) {
+            if (array[meio] == numero) {
+                localizador = true;
+            } else {
+                if (numero < array[meio]) {
+                    fim = meio - 1;
+                } else {
+                    inicio = meio + 1;
+                }
+                meio = (inicio + fim) / 2;
+            }
+        }
+        meio = (inicio + fim) / 2;
+        if (localizador) {
+            System.out.println("O número: " + numero + " já existe.");
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == 0) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+}
